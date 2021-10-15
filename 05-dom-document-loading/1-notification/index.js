d8f8583f8a3a7ac1ccd7904c070213cdb397c7c5
Artur Hazratov <artur.hazratov@gmail.com>
2021-10-16 00:18:43 +0800

    sortable table problem

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+  constructor (message, {duration = 0, type = ''} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+  static activeNote;
+   element;
 
+   get template () {
+     return `<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>`;
+   }
+
+   render () {
+     const msgDiv = document.createElement('div');
+     msgDiv.innerHTML = this.template;
+     this.element = msgDiv.firstElementChild;
+   }
+
+   show (target = document.body) {
+     if (NotificationMessage.activeNote) {
+       NotificationMessage.activeNote.remove()
+     }
+     
+     target.append(this.element);
+     setTimeout(()=>{
+       this.remove();
+     }, this.duration);
+     NotificationMessage.activeNote = this;
+
+   }
+
+   remove() {
+     if (this.element) {
+       this.element.remove();
+     }
+     
+   }
+
+   destroy () {
+     this.element = null;
+     this.message = '';
+     this.remove();
+   }
 }
